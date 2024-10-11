59a0550f69b85b8449de1f1ddd879123ad25808d
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2024-10-11 18:38:15 +0200

    Aggiunti header, footer e index.php

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-require_once('php/backend/service_manager.php');
-$title = "Le migliori onoranze ad Altivole - Onoranze Funebri Stecca";
-$page = "index";
-$description = "Onoranze Funebri Stecca, il forno crematorio e la casa funeraria fanno parte delle eccellenze funerarie di Altivole.";
-$keywords = "onoranze, Onoranze Stecca, onoranze funebri, Altivole, onoranze Altivole, crematorio, casa funeraria, funerale";
-
-include "php/template/header.php";
-$content = "";
-$DOM = file_get_contents('html/home.html');
-echo ($DOM);
-include "php/template/footer.php";
+echo(file_get_contents('HTML/template/header.html'));
+echo(file_get_contents('index.html'));
+echo(file_get_contents('HTML/footer.html'));
 ?>
