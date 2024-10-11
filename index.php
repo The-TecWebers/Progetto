da846ce4d80bcc878ab7908ce106dc291d873a27
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2024-10-11 20:55:17 +0200

    Fix minori

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo(file_get_contents('HTML/template/header.html'));
-echo(file_get_contents('index.html'));
-echo(file_get_contents('HTML/footer.html'));
+echo (file_get_contents('HTML/template/header.html'));
+echo (file_get_contents('index.html'));
+echo (file_get_contents('HTML/template/footer.html'));
 ?>
