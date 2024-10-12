93b75ebfb9e101ca9a5668a126d5a535693f1531
Riccardo Stefani <riccardost02@gmail.com>
2024-10-12 22:22:28 +0200

    Sistemazione file connessione server e creazione database

--- PHP/db_create.php
+++ PHP/db_create.php
@@ -1,12 +1,18 @@
 <?php
 require('db_connect.php');
 // Create database
-$sql = "CREATE DATABASE OneSports";
+$sql = "CREATE DATABASE IF NOT EXISTS OneSports";
+
+if ($conn->query($sql) !== TRUE) {
+  die("Error creating database: " . $conn->error);
+}
+/*
 if ($conn->query($sql) === TRUE) {
   echo "Database created successfully";
 } else {
   echo "Error creating database: " . $conn->error;
 }
+*/
 
 $conn->close();
 ?>
