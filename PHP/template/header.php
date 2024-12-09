b17c07d9d81d636e363d64da94d3e6704a3cf3d3
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2024-12-09 16:52:29 +0100

    Aggiunti link attivi dinamici

--- PHP/template/header.php
+++ PHP/template/header.php
@@ -6,6 +6,19 @@ $header = file_get_contents($path);
 $header = str_replace('<title></title>', '<title>' . $titolo . '</title>', $header);
 $header = str_replace('<meta name="description" content=""/>', '<meta name="description" content="' . $descrizione . '"/>', $header);
 $header = str_replace('<meta name="keywords" content=""/>', '<meta name="keywords" content="' . $keywords . '" />', $header);
+
+$current_page = basename($_SERVER['PHP_SELF']);
+
+switch ($current_page) {
+    case "index.php":
+        $header = str_replace('<li><a href="/index.php">Home</a></li>', '<li><a class="active" href="/index.php">Home</a></li>', $header);
+        break;
+    case "preventivi.php":
+        $header = str_replace('<li><a href="/preventivi.php">Preventivi</a></li>', '<li><a class="active" href="/preventivi.php">Preventivi</a></li>', $header);
+        break;
+    default:
+        break;
+}
 echo($header);
 
 
