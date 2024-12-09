407a6d8d1097d81a5720f4a9899da1aca7233643
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2024-12-09 17:03:45 +0100

    Rimossi link assoluti, aggiunto jquery

--- PHP/template/header.php
+++ PHP/template/header.php
@@ -11,10 +11,12 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
 switch ($current_page) {
     case "index.php":
-        $header = str_replace('<li><a href="/index.php">Home</a></li>', '<li><a class="active" href="/index.php">Home</a></li>', $header);
+        $header = str_replace('<li><a href="index.php">Home</a></li>', '<li><a class="active" href="/index.php">Home</a></li>', $header);
         break;
     case "preventivi.php":
-        $header = str_replace('<li><a href="/preventivi.php">Preventivi</a></li>', '<li><a class="active" href="/preventivi.php">Preventivi</a></li>', $header);
+        $header = str_replace('<li><a href="preventivi.php">Preventivi</a></li>', '<li><a class="active" href="/preventivi.php">Preventivi</a></li>', $header);
+        $header = str_replace('<script></script>', '<script src="./../../JS/jquery.js" defer></script>', $header);
+        $header = str_replace('<script></script>', '<script src="./../../JS/preventivi.js" defer></script>', $header);
         break;
     default:
         break;
