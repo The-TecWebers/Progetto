64af40d652e561f4ca2f9958bfcdaf03905d7af7
Matteo Mazzotti <matteo.mazzotti@outlook.it>
2024-12-11 18:25:08 +0100

    Tolti link circolari su header.php, convertito codice preventivi.js in JavaScript puro

--- JS/preventivi.js
+++ JS/preventivi.js
@@ -1,20 +1,19 @@
-$(document).ready(function(){
-    creaPreventivo();
-    $("#caricaPreventivi").on('click',mostraPreventivi);
-    $("#creaPreventivo").on('click',creaPreventivo);
+document.addEventListener('DOMContentLoaded', function() {
+    creaPreventivo(); 
+    document.getElementById('caricaPreventivi').addEventListener('click', mostraPreventivi); 
+    document.getElementById('creaPreventivo').addEventListener('click', creaPreventivo); 
 });
 
-function creaPreventivo(){
-    $("#listaPreventivi").addClass('hidden');
-    $("#form-preventivi").removeClass('hidden');
-    $("#creaPreventivo").removeClass('outline-green');
-    $("#caricaPreventivi").addClass('outline-green');
+function creaPreventivo() {
+    document.getElementById('listaPreventivi').classList.add('hidden');
+    document.getElementById('form-preventivi').classList.remove('hidden');
+    document.getElementById('creaPreventivo').classList.remove('outline-green');
+    document.getElementById('caricaPreventivi').classList.add('outline-green');
 }
 
-function mostraPreventivi()
-{
-    $("#listaPreventivi").removeClass('hidden');
-    $("#form-preventivi").addClass('hidden');
-    $("#creaPreventivo").addClass('outline-green');
-    $("#caricaPreventivi").removeClass('outline-green');
+function mostraPreventivi() {
+    document.getElementById('listaPreventivi').classList.remove('hidden');
+    document.getElementById('form-preventivi').classList.add('hidden');
+    document.getElementById('creaPreventivo').classList.add('outline-green');
+    document.getElementById('caricaPreventivi').classList.remove('outline-green');
 }
--- PHP/template/header.php
+++ PHP/template/header.php
@@ -11,10 +11,10 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
 switch ($current_page) {
     case "index.php":
-        $header = str_replace('<li><a href="index.php">Home</a></li>', '<li><a class="active" href="index.php">Home</a></li>', $header);
+        $header = str_replace('<li><a href="index.php">Home</a></li>', '<li class="current-link">Home</li>', $header);
         break;
     case "preventivi.php":
-        $header = str_replace('<li><a href="preventivi.php">Preventivi</a></li>', '<li><a class="active" href="preventivi.php">Preventivi</a></li>', $header);
+        $header = str_replace('<li><a href="preventivi.php">Preventivi</a></li>', '<li class="current-link">Preventivi</li>', $header);
         $header = str_replace('<script></script>', '<script src="JS\jquery.js" defer></script> <script src="JS\preventivi.js" defer></script>', $header);
         break;
     default:
