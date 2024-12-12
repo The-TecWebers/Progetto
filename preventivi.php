d0ff8fcf2d3a20b4fb903ff88fa03ba0dfb58f15
rickyz03 <riccardost02@gmail.com>
2024-12-12 22:25:24 +0100

    Chiusura di tag PHP

--- preventivi.php
+++ preventivi.php
@@ -5,3 +5,4 @@ $keywords = "Scavi, edilizia, scavi brescia, lavori edilizi";
 include "./PHP/template/header.php";
 echo(file_get_contents('./HTML/pages/preventivi.html'));
 include "./PHP/template/footer.php";
+?>
