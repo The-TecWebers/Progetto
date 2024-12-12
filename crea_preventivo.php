0c6ec6d364b29706b9e54d4bd5474b22ff7aabbf
rickyz03 <riccardost02@gmail.com>
2024-12-12 22:23:58 +0100

    Semplice chiusura del tag php con ?>

--- crea_preventivo.php
+++ crea_preventivo.php
@@ -5,3 +5,4 @@ $keywords = "Scavi, edilizia, scavi brescia, lavori edilizi";
 include "./PHP/template/header.php";
 echo(file_get_contents('./HTML/pages/crea_preventivo.html'));
 include "./PHP/template/footer.php";
+?>
