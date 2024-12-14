2e63be93c9bebd9593d54f7e24bf0990d66ea7fe
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-12-14 21:42:15 +0100

    Home fixes

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$titolo = "EdilScavi";
-$descrizione= "EdilScavi SRL Brescia";
+$titolo = "Home - EdilScavi";
+$descrizione= "Home Page di Edil Scavi Srl:";
 $keywords = "Scavi, edilizia, scavi brescia, lavori edilizi";
 include "./PHP/template/header.php";
 echo(file_get_contents('./index.html'));
