7207fe341c1209f5307ec95c4126118b10e51bf6
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2024-12-14 22:10:50 +0100

    fix mezzi

--- mezzi.php
+++ mezzi.php
@@ -1,5 +1,5 @@
 <?php
-$titolo = "EdilScavi";
+$titolo = "Mezzi - EdilScavi";
 $descrizione= "EdilScavi SRL Brescia";
 $keywords = "Scavi, edilizia, scavi brescia, lavori edilizi";
 include "./PHP/template/header.php";
