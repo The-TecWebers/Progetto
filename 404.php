793ecce32f6fae84c1a146ee835d01068827d2af
Carmelo Russello <carmelo@MacBookAir.home>
2024-12-14 17:53:09 +0100

    aggiunte pagine 404, areaprivata e mezzi

---
+++ 404.php
@@ -0,0 +1,8 @@
+<?php
+$titolo = "EdilScavi";
+$descrizione= "EdilScavi SRL Brescia";
+$keywords = "Scavi, edilizia, scavi brescia, lavori edilizi";
+include "./PHP/template/header.php";
+echo(file_get_contents('./HTML/pages/404.html'));
+include "./PHP/template/footer.php";
+?>
---
+++ areaprivata.php
@@ -0,0 +1,8 @@
+<?php
+$titolo = "EdilScavi";
+$descrizione= "EdilScavi SRL Brescia";
+$keywords = "Scavi, edilizia, scavi brescia, lavori edilizi";
+include "./PHP/template/header.php";
+echo(file_get_contents('./HTML/pages/areaprivata.html'));
+include "./PHP/template/footer.php";
+?>
