df9cfa21b03c4b2e53203689f07b9560545b85e3
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2024-12-16 13:42:55 +0100

    Fix minori dopo merge

--- su_di_noi.php
+++ su_di_noi.php
@@ -3,10 +3,6 @@ $titolo = "Preventivi - EdilScavi";
 $descrizione= "EdilScavi SRL Brescia";
 $keywords = "Scavi, edilizia, scavi brescia, lavori edilizi";
 include "./PHP/template/header.php";
-<<<<<<< HEAD:preventivi.php
-echo(file_get_contents('./HTML/pages/preventivi.html'));
-=======
 echo(file_get_contents('./HTML/pages/su_di_noi.html'));
->>>>>>> feature/layout:su_di_noi.php
 include "./PHP/template/footer.php";
 ?>
