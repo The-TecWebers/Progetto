dc4d2a248e5c83e67095aa719105d5a3d706171d
Riccardo Stefani <riccardost02@gmail.com>
2024-12-16 18:03:28 +0100

    Organizzato un po' meglio CSS

--- area_privata.php
+++ area_privata.php
@@ -3,6 +3,6 @@ $titolo = "EdilScavi";
 $descrizione= "EdilScavi SRL Brescia";
 $keywords = "Scavi, edilizia, scavi brescia, lavori edilizi";
 include "./PHP/template/header.php";
-echo(file_get_contents('./HTML/pages/areaprivata.html'));
+echo(file_get_contents('./HTML/pages/area_privata.html'));
 include "./PHP/template/footer.php";
 ?>
