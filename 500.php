168fd947e4b36303cc7d5eb0f534e3d19b84025e
Carmelo Russello <carmelo@MacBookAir.home>
2024-12-18 13:08:13 +0100

    sistemato errore di nomenclatura della pagina di errore 500

--- 500.php
+++ 500.php
@@ -1,9 +1,9 @@
 <?php
-$titolo = "505 - EdilScavi";
+$titolo = "500 - EdilScavi";
 $descrizione= "La pagina che stai cercando non è stata trovata su EdilScavi Srl. Scopri di più sui nostri servizi.";
-$keywords = "errore 505, pagina non trovata, edilizia, scavi brescia, servizi EdilScavi";
+$keywords = "errore 500, pagina non trovata, edilizia, scavi brescia, servizi EdilScavi";
 
 include "./PHP/template/header.php";
-echo(file_get_contents('./HTML/pages/505.html'));
+echo(file_get_contents('./HTML/pages/500.html'));
 include "./PHP/template/footer.php";
 ?>
