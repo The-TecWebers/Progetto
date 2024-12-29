6d5929712f1c17c29b063994f2b4b2ba93cb04e5
rickyz03 <riccardost02@gmail.com>
2024-12-29 21:33:41 +0100

    close #95 Ho reso l'header fisso, senza usare JavaScript. Poi, ho centrato per bene le breadcrumb e ho studiato bene come l'header viene visualizzato

--- PHP/template/header.php
+++ PHP/template/header.php
@@ -26,38 +26,42 @@ $header = str_replace('<meta name="keywords" content=""/>', '<meta name="keyword
 
 $current_page = basename($_SERVER['PHP_SELF']);
 
+$placeholder = '<!-- <breadcrumb-to-replace> -->';
+$prefix = '<nav class="breadcrumb" aria-label="breadcrumb"><ul><li><a href="index.php" lang="en">Home</a></li><li>';
+$suffix = '</li></ul></nav>';
+
 switch ($current_page) {
     case "index.php":
         $header = str_replace('<li><a href="index.php">Home</a></li>', '<li class="current-link">Home</li>', $header);
         break;
     case "storia.php":
         $header = str_replace('<li><a href="storia.php">Storia</a></li>', '<li class="current-link">Storia</li>', $header);
-        $header = str_replace('<!-- <ul-to-replace></ul-to-replace> -->', '<ul><li><a href="index.php" lang="en">Home</a></li><li>Storia</li></ul>', $header);
+        $header = str_replace($placeholder, $prefix . 'Storia' . $suffix, $header);
         break;
     case "mezzi.php":
         $header = str_replace('<li><a href="mezzi.php">Mezzi</a></li>', '<li class="current-link">Mezzi</li>', $header);
-        $header = str_replace('<!-- <ul-to-replace></ul-to-replace> -->', '<ul><li><a href="index.php" lang="en">Home</a></li><li>Mezzi</li></ul>', $header);
+        $header = str_replace($placeholder, $prefix . 'Mezzi' . $suffix, $header);
         break;
     case "lavori_svolti.php":
         $header = str_replace('<li><a href="lavori_svolti.php">Lavori svolti</a></li>', '<li class="current-link">Lavori svolti</li>', $header);
-        $header = str_replace('<!-- <ul-to-replace></ul-to-replace> -->', '<ul><li><a href="index.php" lang="en">Home</a></li><li>Lavori svolti</li></ul>', $header);
+        $header = str_replace($placeholder, $prefix . 'Lavori svolti' . $suffix, $header); 
         break;
     case "lista_preventivi.php":
         $header = str_replace('<li><a href="lista_preventivi.php">Preventivi</a></li>', '<li class="current-link">Preventivi</li>', $header);
-        $header = str_replace('<!-- <ul-to-replace></ul-to-replace> -->', '<ul><li><a href="index.php" lang="en">Home</a></li><li>Preventivi</li></ul>', $header);
+        $header = str_replace($placeholder, $prefix . 'Preventivi' . $suffix, $header); 
         break;
     case "su_di_noi.php":
         $header = str_replace('<li><a href="su_di_noi.php">Su di noi</a></li>', '<li class="current-link">Su di noi</li>', $header);
-        $header = str_replace('<!-- <ul-to-replace></ul-to-replace> -->', '<ul><li><a href="index.php" lang="en">Home</a></li><li>Su di noi</li></ul>', $header);
+        $header = str_replace($placeholder, $prefix . 'Su di noi' . $suffix, $header); 
         break;
     case "registrati.php":
-        $header = str_replace('<!-- <ul-to-replace></ul-to-replace> -->', '<ul><li><a href="index.php" lang="en">Home</a></li><li>Registrati</li></ul>', $header);
+        $header = str_replace($placeholder, $prefix . 'Registrati' . $suffix, $header); 
         break;
     case "accedi.php":
-        $header = str_replace('<!-- <ul-to-replace></ul-to-replace> -->', '<ul><li><a href="index.php" lang="en">Home</a></li><li>Accedi</li></ul>', $header);
+        $header = str_replace($placeholder, $prefix . 'Accedi' . $suffix, $header); 
         break;
     case "area_privata.php":
-        $header = str_replace('<!-- <ul-to-replace></ul-to-replace> -->', '<ul><li><a href="index.php" lang="en">Home</a></li><li>Area privata</li></ul>', $header);
+        $header = str_replace($placeholder, $prefix . 'Area privata' . $suffix, $header); 
         break;
 
     default:
