5d3cc6674f4a922ad131ed7a303be9f5af377b4c
rickyz03 <riccardost02@gmail.com>
2024-12-30 09:53:28 +0100

    Introdotto la chiusura di sessione in caso di normale isLogged(), perchè sennò quando va su header.php ti mostraa schermo la notice che la session è already started

--- lista_preventivi.php
+++ lista_preventivi.php
@@ -6,6 +6,9 @@ $titolo = "Lista preventivi - EdilScavi";
 $descrizione = "Lista dei tuoi preventivi per i lavori di scavi e edilizia con EdilScavi";
 $keywords = "preventivi, scavi, edilizia, scavi brescia, lavori edilizi";
 if (AuthController::isLogged()) {
+    session_write_close();
+    session_abort();
+
     include __DIR__ . DIRECTORY_SEPARATOR . "PHP" . DIRECTORY_SEPARATOR . "template" . DIRECTORY_SEPARATOR . "header.php";
     echo file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "HTML" . DIRECTORY_SEPARATOR . "pages" . DIRECTORY_SEPARATOR . "lista_preventivi.html");
     include __DIR__ . DIRECTORY_SEPARATOR . "PHP" . DIRECTORY_SEPARATOR . "template" . DIRECTORY_SEPARATOR . "footer.php";
