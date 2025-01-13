84c5685e8ef546ccbcec285fd6d91da32b5fbd0a
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2025-01-13 22:25:04 +0100

    keywords

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $titolo = "Home - EdilScavi";
 $descrizione = "Esperti in movimento terra e urbanizzazione ad Artogne, Valle Camonica";
-$keywords = "EdilScavi, costruzioni, edilizia, Artogne, movimento terra, impianti";
+$keywords = "EdilScavi,costruzioni,edilizia,Artogne,movimento terra,impianti";
 
 include __DIR__ . DIRECTORY_SEPARATOR . "PHP" . DIRECTORY_SEPARATOR . "template" . DIRECTORY_SEPARATOR . "header.php";
 echo file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "index.html");
