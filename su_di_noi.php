42dc4ec98acd2178cc1ce6044e84a8d674253a72
Enrico Cotti Cottini <enrico.cotticottini@gmail.com>
2025-01-13 23:38:37 +0100

    risemanticizzato

--- su_di_noi.php
+++ su_di_noi.php
@@ -1,7 +1,8 @@
 <?php
 $titolo = "Su di noi - EdilScavi";
 $descrizione= "Edil Scavi Srl Brescia: i nostri valori, la nostra squadra, i nostri presidenti";
-$keywords = "su di noi, EdilScavi, Cotti Cottini, valori, squadra, presidenti";
+$keywords = "Claudio,Oscar,movimento terra,reti gas metano,saldatura,tubazioni,strutture edilizie";  
+
 
 include __DIR__ . DIRECTORY_SEPARATOR . "PHP" . DIRECTORY_SEPARATOR . "template" . DIRECTORY_SEPARATOR . "header.php";
 echo file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . "HTML" . DIRECTORY_SEPARATOR . "pages" . DIRECTORY_SEPARATOR . "su_di_noi.html");
