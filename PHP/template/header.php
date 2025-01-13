3e43c8616e2b97f67c008b9ca9fe4be2d5bf6fe2
rickyz03 <riccardost02@gmail.com>
2025-01-13 12:20:45 +0100

    Inserito aria-current='page' in menù e breadcrumb, così poi possiamo scriverlo in relazione!

--- PHP/template/header.php
+++ PHP/template/header.php
@@ -25,62 +25,64 @@ $header = str_replace('<meta name="keywords" content="">', '<meta name="keywords
 $current_page = basename($_SERVER['PHP_SELF']);
 
 $placeholder = '<!-- <breadcrumb-to-replace> -->';
-$prefix = '<nav class="breadcrumb" aria-label="breadcrumb"><ul><li><a href="index.php" lang="en">Home</a></li><li>';
-$suffix = '</li></ul></nav>';
+$prefix = '<nav class="breadcrumb" aria-label="Percorso nel sito"><ul><li><a href="index.php" lang="en">Home</a></li>';
+$current = '<li><span aria-current="page">';
+$suffix = '</span></li></ul></nav>';
 
 switch ($current_page) {
     case "index.php":
-        $header = str_replace('<li><a href="index.php" lang="en">Home</a></li>', '<li class="current-link" lang="en">Home</li>', $header);
+        $header = str_replace('<li><a href="index.php" lang="en">Home</a></li>', '<li class="current-link" aria-current="page" lang="en">Home</li>', $header);
         break;
     case "storia.php":
-        $header = str_replace('<li><a href="storia.php">Storia</a></li>', '<li class="current-link">Storia</li>', $header);
-        $header = str_replace($placeholder, $prefix . 'Storia' . $suffix, $header);
+        $header = str_replace('<li><a href="storia.php">Storia</a></li>', '<li class="current-link" aria-current="page">Storia</li>', $header);
+        $header = str_replace($placeholder, $prefix . $current . 'Storia' . $suffix, $header);
         break;
     case "mezzi.php":
-        $header = str_replace('<li><a href="mezzi.php">Mezzi</a></li>', '<li class="current-link">Mezzi</li>', $header);
-        $header = str_replace($placeholder, $prefix . 'Mezzi' . $suffix, $header);
+        $header = str_replace('<li><a href="mezzi.php">Mezzi</a></li>', '<li class="current-link" aria-current="page">Mezzi</li>', $header);
+        $header = str_replace($placeholder, $prefix . $current . 'Mezzi' . $suffix, $header);
         break;
     case "lavori_svolti.php":
-        $header = str_replace('<li><a href="lavori_svolti.php">Lavori svolti</a></li>', '<li class="current-link">Lavori svolti</li>', $header);
-        $header = str_replace($placeholder, $prefix . 'Lavori svolti' . $suffix, $header);
+        $header = str_replace('<li><a href="lavori_svolti.php">Lavori svolti</a></li>', '<li class="current-link" aria-current="page">Lavori svolti</li>', $header);
+        $header = str_replace($placeholder, $prefix . $current . 'Lavori svolti' . $suffix, $header);
         break;
     case "lista_preventivi.php":
-        $header = str_replace('<li><a href="lista_preventivi.php">Preventivi</a></li>', '<li class="current-link">Preventivi</li>', $header);
-        $header = str_replace($placeholder, $prefix . 'Preventivi' . $suffix, $header);
+        $header = str_replace('<li><a href="lista_preventivi.php">Preventivi</a></li>', '<li class="current-link" aria-current="page">Preventivi</li>', $header);
+        $header = str_replace($placeholder, $prefix . $current . 'Preventivi' . $suffix, $header);
         break;
     case "su_di_noi.php":
-        $header = str_replace('<li><a href="su_di_noi.php">Su di noi</a></li>', '<li class="current-link">Su di noi</li>', $header);
-        $header = str_replace($placeholder, $prefix . 'Su di noi' . $suffix, $header);
+        $header = str_replace('<li><a href="su_di_noi.php">Su di noi</a></li>', '<li class="current-link" aria-current="page">Su di noi</li>', $header);
+        $header = str_replace($placeholder, $prefix . $current . 'Su di noi' . $suffix, $header);
         break;
     case "registrati.php":
-        $header = str_replace($placeholder, $prefix . 'Registrati' . $suffix, $header);
         if(isset($_GET['intended']) && $_GET['intended']=='lista_preventivi')
         {
-            $header = str_replace('<li><a href="lista_preventivi.php">Preventivi</a></li>', '<li class="current-link">Preventivi</li>', $header);
+            $header = str_replace('<li><a href="lista_preventivi.php">Preventivi</a></li>', '<li class="current-link" aria-current="page">Preventivi</li>', $header);
         }else{
-            $header = str_replace('<a id="btn-register" href="registrati.php">Registrati</a>', '<div id="btn-register" class="current-link">Registrati</div>', $header);
+            $header = str_replace('<a id="btn-register" href="registrati.php">Registrati</a>', '<div id="btn-register" class="current-link" aria-current="page">Registrati</div>', $header);
         }
+        $header = str_replace($placeholder, $prefix . $current . 'Registrati' . $suffix, $header);
         break;
     case "accedi.php":
-        $header = str_replace($placeholder, $prefix . 'Accedi' . $suffix, $header);
         if(isset($_GET['intended']) && $_GET['intended']=='lista_preventivi')
         {
-            $header = str_replace('<li><a href="lista_preventivi.php">Preventivi</a></li>', '<li class="current-link">Preventivi</li>', $header);
+            $header = str_replace('<li><a href="lista_preventivi.php">Preventivi</a></li>', '<li class="current-link" aria-current="page">Preventivi</li>', $header);
         }else{
-            $header = str_replace('<a id="btn-login" href="accedi.php">Accedi</a>', '<div id="btn-login" class="current-link">Accedi</div>', $header);
+            $header = str_replace('<a id="btn-login" href="accedi.php">Accedi</a>', '<div id="btn-login" class="current-link" aria-current="page">Accedi</div>', $header);
         }
+        $header = str_replace($placeholder, $prefix . $current . 'Accedi' . $suffix, $header);
         break;
     case "area_privata.php":
-        $header = str_replace('<a id="btn-private_area" href="area_privata.php" title="Vai all\'area privata">' . htmlspecialchars($_SESSION['nome']) . '</a>', '<div id="btn-private_area" class="current-link">' . htmlspecialchars($_SESSION['nome']) . '</div>', $header);
-        $header = str_replace($placeholder, $prefix . 'Area privata' . $suffix, $header);
+        $header = str_replace('<a id="btn-private_area" href="area_privata.php" title="Vai all\'area privata">' . htmlspecialchars($_SESSION['nome']) . 
+        '</a>', '<div id="btn-private_area" class="current-link" aria-current="page">' . htmlspecialchars($_SESSION['nome']) . '</div>', $header);
+        $header = str_replace($placeholder, $prefix . $current . 'Area privata' . $suffix, $header);
         break;
     case "crea_preventivo.php":
-        $header = str_replace($placeholder, $prefix . '<a href="lista_preventivi.php">Preventivi</a></li>'
-                  . '<li>Crea preventivo' . $suffix, $header);
+        $header = str_replace($placeholder, $prefix . '<li><a href="lista_preventivi.php">Preventivi</a></li>'
+                  . $current . 'Crea preventivo' . $suffix, $header);
         break;
     case "singolo_preventivo.php":
-        $header = str_replace($placeholder, $prefix . '<a href="lista_preventivi.php">Preventivi</a></li>'
-                  . '<li>Preventivo Utente' . $suffix, $header);
+        $header = str_replace($placeholder, $prefix . '<li><a href="lista_preventivi.php">Preventivi</a></li>'
+                  . $current . 'Preventivo Utente' . $suffix, $header);
         break;
 
     default:
