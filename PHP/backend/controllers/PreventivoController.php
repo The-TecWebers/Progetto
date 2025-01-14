985eb9209d62bbb12b9651b94509e86d643d8ddd
Carmelo Russello <carmelo@MacBookAir.home>
2025-01-14 18:20:36 +0100

    aggiunto css per <p> in caso di assenza preventivi e sistemato padding breadcrumb

--- PHP/backend/controllers/PreventivoController.php
+++ PHP/backend/controllers/PreventivoController.php
@@ -62,7 +62,7 @@ class PreventivoController
         $preventivi = DBController::getPreventivi("SELECT * FROM richiesta_preventivo WHERE utente = ?", $utente->getId());
 
         if (!$preventivi) {
-            return "<p class=''>Non ci sono preventivi da mostrare</p>";
+            return "<p class='message-preventivo'>Non ci sono preventivi da mostrare</p>";
         }
 
         $div = "<div class'grid cols-1'>";
@@ -114,7 +114,7 @@ class PreventivoController
         $preventivi = DBController::getPreventivi("SELECT * FROM richiesta_preventivo");
 
         if (!$preventivi) {
-            return "<p class=''>Non ci sono preventivi da mostrare</p>";
+            return "<p class='message-preventivo'>Non ci sono preventivi da mostrare</p>";
         }
 
         foreach ($preventivi as &$preventivo) { // Usa "&" per passare per riferimento
@@ -198,7 +198,7 @@ class PreventivoController
         $preventivi = DBController::getPreventivi("SELECT * FROM richiesta_preventivo");
 
         if (!$preventivi) {
-            return "<p class=''>Non ci sono preventivi da mostrare</p>";
+            return "<p class='message-preventivo'>Non ci sono preventivi da mostrare</p>";
         }
 
     $div = "<div class='grid cols-1'>";
@@ -241,7 +241,7 @@ class PreventivoController
         }
     }
     if (!$found) {
-        $div .= "<p>Preventivo non trovato.</p>";
+        $div .= "<p class='message-preventivo'>Preventivo non trovato.</p>";
     }
 
     return $div . "</div>";
