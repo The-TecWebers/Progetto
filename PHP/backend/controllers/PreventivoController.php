237fc686772c5582efc0398927798f4d2b169fdb
Matteo Mazzotti <Matteo.Mazzotti@outlook.it>
2025-01-22 21:09:55 +0100

    Tolti id

--- PHP/backend/controllers/PreventivoController.php
+++ PHP/backend/controllers/PreventivoController.php
@@ -118,13 +118,13 @@ class PreventivoController
             <div class='form-preventivo'>
                 <form method='GET' action='preventivi.php'>
                     <input type='hidden' name='action' value='edit'/>
-                    <input type='hidden' id='edit_preventivo_id' name='edit_preventivo_id' value='" . $preventivo['id'] . "'/>
+                    <input type='hidden' name='edit_preventivo_id' value='" . $preventivo['id'] . "'/>
                     <button type='submit' aria-label='Modifica preventivo'>
                         <img alt='' src='Images/icons/edit_white.svg' height=30 width=30>
                     </button>
                 </form>
                 <form method='POST' action='preventivi.php?action=delete'>
-                      <input type='hidden' id='delete_preventivo_id' name='delete_preventivo_id' value='" . $preventivo['id'] . "'/>
+                      <input type='hidden' name='delete_preventivo_id' value='" . $preventivo['id'] . "'/>
                     <button type='submit' aria-label='Elimina preventivo'>
                         <img alt='' src='Images/icons/delete_white.svg' height=30 width=30>
                     </button>
@@ -269,7 +269,7 @@ class PreventivoController
         </div>
              <div class='form-preventivo'>
                   <form method='POST' action='preventivi.php?action=delete'>
-                      <input type='hidden' id='delete_preventivo_id' name='delete_preventivo_id' value='" . $preventivo['id'] . "'/>
+                      <input type='hidden' name='delete_preventivo_id' value='" . $preventivo['id'] . "'/>
                       <button type='submit' aria-label='Elimina preventivo'>
                           <img alt='' src='Images/icons/delete_white.svg' height=30 width=30>
                       </button>
